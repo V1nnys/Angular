4fcad2e2af09ca253fc073adf86e3825cdb16bb4
Vinicius Silva <vinicius.silva.89@outlook.com>
2023-09-13 15:41:26 +0100

    two-way-binding, services, routing

--- project1/src/app/app-routing.module.ts
+++ project1/src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+
+const routes: Routes = [
+  {path: 'list', component: ListRenderComponent},
+  {path: 'event', component: EventosComponent},
+  {path: 'emitter', component: EmitterComponent},
+  {path: 'pipes', component: PipesComponent}
+
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
