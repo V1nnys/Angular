2ac8baa6ec1e2bff1aad6217dfc302ef34b24bf1
Vinicius Silva <vinicius.silva.89@outlook.com>
2023-10-21 19:51:39 +0100

    form.validation.share

--- moments.angular/moments/src/app/components/moment-form/moment-form.component.ts
+++ moments.angular/moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
@@ -8,5 +10,32 @@ import { Component, Input } from '@angular/core';
 export class MomentFormComponent {
 
   @Input() btnText!: string;
+
+  momentForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl('', [Validators.required]),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl(''),
+      image: new FormControl(''),
+    })
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+
+    console.log('Enviou formulário');
+  }
 
 }
