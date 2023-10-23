1137526bda11895b04f656e5b2949bbfa555b859
Vinicius Silva <vinicius.silva.89@outlook.com>
2023-10-23 15:04:57 +0100

    system.data

--- moments.angular/moments/src/app/components/moment-form/moment-form.component.ts
+++ moments.angular/moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Moment } from 'src/app/Moments';
 
 @Component({
   selector: 'app-moment-form',
@@ -8,7 +10,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./moment-form.component.css']
 })
 export class MomentFormComponent {
-
+  @Output() onSubmit = new EventEmitter<Moment>()
   @Input() btnText!: string;
 
   momentForm!: FormGroup;
@@ -30,12 +32,22 @@ export class MomentFormComponent {
     return this.momentForm.get('description')!;
   }
 
+  onFileSelected(event: any) {
+
+    const file: File = event.target.files[0]
+
+    this.momentForm.patchValue({image: file})
+
+  }
+
   submit() {
     if (this.momentForm.invalid) {
       return;
     }
 
-    console.log('Enviou formulário');
+    console.log(this.momentForm.value);
+
+    this.onSubmit.emit(this.momentForm.value);
   }
 
 }
