2fd96043241a481eac51dd0b8a478963a6b2e265
Vinicius Silva <vinicius.silva.89@outlook.com>
2023-11-16 14:17:01 +0000

    Udemy
    The code from udemy's classes and some tests.

---
+++ Angular2+/meu-projeto-udemy/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-title title="Olá Mundo"></app-title>
+    <router-outlet></router-outlet>,
+  `
+})
+export class AppComponent {
+  
+  constructor() {}
+
+  ngOnInit(): void {
+    setTimeout(()=> {
+      console.log(1)
+    }, 5000)
+    
+  };
+
+}
