9d5c9b965ce5a226a8d66df18ea101c86709981e
Vinicius Silva <vinicius.silva.89@outlook.com>
2023-11-23 16:23:56 +0000

    angular.cicles
    DoCheck,
      AfterContentInit,
      AfterContentChecked,
      AfterViewInit,
      AfterViewChecked

--- Angular2+/meu-projeto-udemy/src/app/title/title.component.ts
+++ Angular2+/meu-projeto-udemy/src/app/title/title.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent implements OnInit, OnChanges {
+export class TitleComponent implements OnInit, OnChanges, OnDestroy {
 
   @Input() public title:string = "Bem-vindo!";
 
@@ -14,7 +14,10 @@ export class TitleComponent implements OnInit, OnChanges {
   ngOnInit(): void {}
 
   ngOnChanges(): void {
-    alert("Foi alterado com sucesso.")
+  }
+
+  ngOnDestroy(): void {
+    console.log("Componente destruído.")
   }
 
 }
