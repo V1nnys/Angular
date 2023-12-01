a912ee71ff1edf406e2c0aa8de2afdcfed54cdfb
Vinicius Silva <vinicius.silva.89@outlook.com>
2023-12-01 12:36:12 +0000

    property.binding+event.binding

--- Angular2+/meu-projeto-udemy/src/app/data-binding/data-binding.component.ts
+++ Angular2+/meu-projeto-udemy/src/app/data-binding/data-binding.component.ts
@@ -10,4 +10,24 @@ export class DataBindingComponent {
   public nome: string = "Vinis";
   public idade: number = 34;
   public maisUm: number = 1;
+
+  public checkedDisabled: boolean = false;
+  public imgSrc: string = "https://images.app.goo.gl/RXYS1bdYFyZVJj2i8";
+  public imgTitle: string = "property Binding";
+
+  public position: { x: number, y: number } = { x: 0, y: 0 }
+  //public position: any;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public alertaInfo(valor: MouseEvent) {
+    console.log(valor);
+  }
+
+  public mouseMoveTeste(valor: MouseEvent){
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
+
 }
